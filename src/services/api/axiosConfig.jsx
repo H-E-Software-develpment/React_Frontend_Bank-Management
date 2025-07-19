5d07f8076f1ea5267468a55ba564c7a2e00230c0
topito123 <guillermoherrera2006@gmail.com>
2025-07-19 00:41:56 -0600

    feat(Axios): Actualizacion de ruta para el backend

--- src/services/api/axiosConfig.jsx
+++ src/services/api/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3001/bankManagement/v1",
+  baseURL: "https://node-js-backend-bank-management.vercel.app/bankManagement/v1",
 });
 
 api.interceptors.request.use((config) => {
